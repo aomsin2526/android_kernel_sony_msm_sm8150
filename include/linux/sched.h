Sony-Kumano-Platform-Development/android_kernel_sony_sm8150

commit 16532038b6928981f808e32d0954d4c65acad686

Import changes from 55.2.A.4.229 for Xperia™ 1 and Xperia™ 5

include/linux/sched.h
@@ -1575,10 +1575,11 @@ extern struct pid *cad_pid;
 #define PF_RANDOMIZE		0x00400000	/* Randomize virtual address space */
 #define PF_SWAPWRITE		0x00800000	/* Allowed to write to swap */
 #define PF_MEMSTALL		0x01000000	/* Stalled due to lack of memory */
+#define PF_MIGRATE_ED_TASK	0x02000000	/* Early detection task migration */
 #define PF_NO_SETAFFINITY	0x04000000	/* Userland is not allowed to meddle with cpus_allowed */
 #define PF_MCE_EARLY		0x08000000      /* Early kill for mce process policy */
 #define PF_WAKE_UP_IDLE         0x10000000	/* TTWU on an idle CPU */
-#define PF_MUTEX_TESTER		0x20000000	/* Thread belongs to the rt mutex tester */
+#define PF_MIGRATE_CUM_ADJ_TASK	0x20000000	/* Cumulative task adjustment should be done */
 #define PF_FREEZER_SKIP		0x40000000	/* Freezer should not count it as freezable */
 #define PF_SUSPEND_TASK		0x80000000      /* This thread called freeze_processes() and should not be frozen */
 

kernel/sched/sched.h
@@ -792,6 +792,7 @@ struct rq {
 #endif
 	#define CPU_LOAD_IDX_MAX 5
 	unsigned long cpu_load[CPU_LOAD_IDX_MAX];
+	unsigned int nr_pinned_tasks;
 #ifdef CONFIG_NO_HZ_COMMON
 #ifdef CONFIG_SMP
 	unsigned long last_load_update_tick;
@@ -900,8 +901,6 @@ struct rq {
 	int prev_top;
 	int curr_top;
 	bool notif_pending;
-	u64 last_cc_update;
-	u64 cycles;
 #endif /* CONFIG_SCHED_WALT */
 
 #ifdef CONFIG_IRQ_TIME_ACCOUNTING
@@ -2409,6 +2408,7 @@ enum rq_nohz_flag_bits {
 
 #define nohz_flags(cpu)	(&cpu_rq(cpu)->nohz_flags)
 
+DECLARE_PER_CPU(atomic_t, claim_wake_up_cpu);
 extern void nohz_balance_exit_idle(unsigned int cpu);
 #else
 static inline void nohz_balance_exit_idle(unsigned int cpu) { }

aomsin2526/android_kernel_sony_msm_sm8150

commit 9b7237cb80cddc5cd124c35560c223f85c7acd7c

sched: Change PF_MIGRATE_ED_TASK flag
The flag was introduced by Sony in its kumano import. It has
questionable use, but it is worthwhile to change the flag itself since it
will conflict with the future introduction of usermodehelper processes.

Fixes: 16532038b6928981f808 ("Import changes from 55.2.A.4.229 for Xperia™ 1 and Xperia™ 5")
Signed-off-by: voidanix <voidanix@keyedlimepie.org>

include/linux/sched.h
@@ -1574,6 +1574,7 @@ extern struct pid *cad_pid;
 #define PF_MEMALLOC		0x00000800	/* Allocating memory */
 #define PF_NPROC_EXCEEDED	0x00001000	/* set_user() noticed that RLIMIT_NPROC was exceeded */
 #define PF_USED_MATH		0x00002000	/* If unset the fpu must be initialized before use */
+#define PF_MIGRATE_ED_TASK	0x00004000	/* Early detection task migration */
 #define PF_NOFREEZE		0x00008000	/* This thread should not be frozen */
 #define PF_FROZEN		0x00010000	/* Frozen for system suspend */
 #define PF_KSWAPD		0x00020000	/* I am kswapd */
@@ -1584,7 +1585,6 @@ extern struct pid *cad_pid;
 #define PF_RANDOMIZE		0x00400000	/* Randomize virtual address space */
 #define PF_SWAPWRITE		0x00800000	/* Allowed to write to swap */
 #define PF_MEMSTALL		0x01000000	/* Stalled due to lack of memory */
-#define PF_MIGRATE_ED_TASK	0x02000000	/* Early detection task migration */
 #define PF_NO_SETAFFINITY	0x04000000	/* Userland is not allowed to meddle with cpus_allowed */
 #define PF_MCE_EARLY		0x08000000      /* Early kill for mce process policy */
 #define PF_WAKE_UP_IDLE         0x10000000	/* TTWU on an idle CPU */